67d47e5a3f46f59e5b28654bb521e4c01c2fc389
lavpreet <lsv2885655@gmail.com>
2023-08-27 20:47:34 +0530

    added changes

--- Django-backend/iNotebookBackend/iNotebookBackend/settings.py
+++ Django-backend/iNotebookBackend/iNotebookBackend/settings.py
@@ -120,6 +120,18 @@ DATABASES = {
 #     }
 # }
 
+import os
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
