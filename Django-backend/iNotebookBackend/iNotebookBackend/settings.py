94b3b9dbacc05ec69517c9f5f3723e32989b7700
lavpreet <lsv2885655@gmail.com>
2023-08-27 21:02:52 +0530

    added db settings

--- Django-backend/iNotebookBackend/iNotebookBackend/settings.py
+++ Django-backend/iNotebookBackend/iNotebookBackend/settings.py
@@ -124,11 +124,11 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT'),
+        "NAME": os.environ.get('PGDATABASE'),
+        "USER": os.environ.get('PGUSER'),
+        "PASSWORD": os.environ.get('PGPASSWORD'),
+        "HOST": os.environ.get('PGHOST'),
+        "PORT": os.environ.get('PGPORT'),
     }
 }
 
