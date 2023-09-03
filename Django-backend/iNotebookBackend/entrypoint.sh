d3aadc8744b2bfa4dce400535bc9113bea30cb8b
lavpreet <lsv2885655@gmail.com>
2023-09-03 15:11:21 +0530

    deploy changes

--- Django-backend/iNotebookBackend/entrypoint.sh
+++ Django-backend/iNotebookBackend/entrypoint.sh
@@ -6,6 +6,11 @@ python3 manage.py collectstatic --no-input
 echo "Creating superuser"
 python manage.py superusercreation
 # Start server
+
+# Migrate database
+echo "Migrate database"
+python manage.py migrate
+
 echo "Starting server"
 # python3 manage.py runserver 0.0.0.0:8000
 
